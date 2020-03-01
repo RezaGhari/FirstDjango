28fc859af1bf98effd55961c79a62059b5e4cf76
92Reza <reza.ghari1995@gmail.com>
2020-03-01 22:24:02 +0330

    Added index view and template to show list of all pens.

--- fptracker/pens/views.py
+++ fptracker/pens/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from pens.models import Pen
 
 # Create your views here.
+
+def index(request):
+
+    all_pens = Pen.objects.all()
+
+    context = {
+        'pen_list': all_pens,
+        'page_title': "Fountain Pen Collection"
+    }
+    return render(request, "index.html", context)
